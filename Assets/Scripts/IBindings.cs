c2d4a7db9b962c6f512f0edf043833fb3695b6f0
BeardedPlatypus <mwtegelaers@gmail.com>
2021-10-30 23:52:02 +0200

    Add IBindings interface

---
+++ Assets/Scripts/IBindings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace BeardedPlatypus.Camera
+{
+    /// <summary>
+    /// <see cref="IBindings"/> defines the bindings used within the controller
+    /// to control the camera location.
+    /// </summary>
+    public interface IBindings
+    {
+        /// <summary>
+        /// Gets the Orbit as an observable <see cref="Vector2"/>.
+        /// </summary>
+        public IObservable<Vector2> Orbit { get; }
+        
+        /// <summary>
+        /// Gets the Translation as an observable <see cref="Vector3"/>.
+        /// </summary>
+        public IObservable<Vector3> Translate { get; }
+        
+        /// <summary>
+        /// Gets the Zoom as an observable <see cref="float"/>.
+        /// </summary>
+        public IObservable<float> Zoom { get; }
+    }
+}
