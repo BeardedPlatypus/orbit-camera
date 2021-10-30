64229f9ea4c83dcdfa569b5f201a69835f904a24
BeardedPlatypus <mwtegelaers@gmail.com>
2021-10-30 23:52:23 +0200

    Add IOrbitSettings

---
+++ Assets/Scripts/CameraSettings/IOrbitSettings.cs
@@ -0,0 +1,24 @@
+﻿namespace BeardedPlatypus.Camera.CameraSettings
+{
+    /// <summary>
+    /// <see cref="IOrbitSettings"/> defines the settings related to the orbit
+    /// behaviour of the Camera.
+    /// </summary>
+    public interface IOrbitSettings
+    {
+        /// <summary>
+        /// Gets the orbit factor.
+        /// </summary>
+        public float Factor { get; }
+
+        /// <summary>
+        /// Gets the range of valid rotations values around the x-axis in degrees.
+        /// </summary>
+        public (float Min, float Max) RangeX { get; }
+        
+        /// <summary>
+        /// Gets the range of valid rotations values around the y-axis in degrees.
+        /// </summary>
+        public (float Min, float Max) RangeY { get; }
+    }
+}
