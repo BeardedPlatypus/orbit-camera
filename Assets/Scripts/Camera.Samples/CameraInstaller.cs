eec826c7bf5603ba07637916544cb059f683d1b2
BeardedPlatypus <mwtegelaers@gmail.com>
2021-11-01 13:40:27 +0100

    Move the CameraSettings to the CameraSettingsInstaller

--- Assets/Scripts/Camera.Samples/CameraInstaller.cs
+++ Assets/Scripts/Camera.Samples/CameraInstaller.cs
@@ -1,5 +1,3 @@
-using BeardedPlatypus.Camera.SettingsComponents;
-using UnityEngine;
 using Zenject;
 
 namespace BeardedPlatypus.Camera.Samples
@@ -10,24 +8,9 @@ namespace BeardedPlatypus.Camera.Samples
     /// </summary>
     public class CameraInstaller : MonoInstaller
     {
-        [SerializeField] private OrbitSettingsScriptableObject orbitSettings;
-        [SerializeField] private TranslationSettingsScriptableObject translationSettings;
-        [SerializeField] private ZoomSettingsScriptableObject zoomSettings;
-        
         public override void InstallBindings()
         {
-            Container.Bind<IOrbitSettings>()
-                     .FromInstance(orbitSettings)
-                     .AsSingle();
-            Container.Bind<ITranslationSettings>()
-                     .FromInstance(translationSettings)
-                     .AsSingle();
-            Container.Bind<IZoomSettings>()
-                     .FromInstance(zoomSettings)
-                     .AsSingle();
-
             Container.Bind<IBindings>().To<Bindings>().FromNewComponentOnNewGameObject().AsSingle();
-            Container.Bind<Settings>().To<Settings>().AsSingle();
             Container.Bind<Controller>().To<Controller>().AsSingle();
         }
     }
---
+++ Assets/Scripts/Camera.Zenject/CameraSettingsInstaller.cs
@@ -0,0 +1,33 @@
+using BeardedPlatypus.Camera.SettingsComponents;
+using UnityEngine;
+using Zenject;
+
+namespace BeardedPlatypus.Camera.Zenject
+{
+    /// <summary>
+    /// <see cref="CameraSettingsInstaller"/> provides the configured settings
+    /// scriptable objects as well as the composed <see cref="Settings"/> as
+    /// dependencies for other objects.
+    /// </summary>
+    public class CameraSettingsInstaller : MonoInstaller
+    {
+        [SerializeField] private OrbitSettingsScriptableObject orbitSettings;
+        [SerializeField] private TranslationSettingsScriptableObject translationSettings;
+        [SerializeField] private ZoomSettingsScriptableObject zoomSettings;
+        
+        public override void InstallBindings()
+        {
+            Container.Bind<IOrbitSettings>()
+                     .FromInstance(orbitSettings)
+                     .AsSingle();
+            Container.Bind<ITranslationSettings>()
+                     .FromInstance(translationSettings)
+                     .AsSingle();
+            Container.Bind<IZoomSettings>()
+                     .FromInstance(zoomSettings)
+                     .AsSingle();
+            
+            Container.Bind<Settings>().To<Settings>().AsSingle();
+        }
+    }
+}
