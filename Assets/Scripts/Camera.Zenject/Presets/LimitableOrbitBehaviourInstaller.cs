7196ecd2d041e9dcabd99677ddb890750f7242dd
BeardedPlatypus <mwtegelaers@gmail.com>
2021-11-07 22:05:44 +0100

    Add LimitableOrbitBehaviourInstaller

---
+++ Assets/Scripts/Camera.Zenject/Presets/LimitableOrbitBehaviourInstaller.cs
@@ -0,0 +1,27 @@
+﻿using BeardedPlatypus.Camera.Core;
+using BeardedPlatypus.Camera.Presets.Orbit;
+using JetBrains.Annotations;
+using UnityEngine;
+using Zenject;
+
+namespace BeardedPlatypus.Camera.Zenject.Presets
+{
+    /// <summary>
+    /// <see cref="LimitableOrbitBehaviourInstaller"/> provides the <see cref="IOrbitBehaviour"/>
+    /// dependency as a <see cref="LimitableOrbitBehaviour"/>.
+    /// </summary>
+    public sealed class LimitableOrbitBehaviourInstaller : MonoInstaller
+    {
+        [SerializeField] [CanBeNull] private LimitableOrbitSettingsScriptableObject settings;
+        
+        public override void InstallBindings()
+        {
+            Container.Bind<ILimitableOrbitSettings>()
+                     .FromInstance(settings)
+                     .AsSingle();
+            Container.Bind<IOrbitBehaviour>()
+                     .To<LimitableOrbitBehaviour>()
+                     .AsSingle();
+        }
+    }
+}
