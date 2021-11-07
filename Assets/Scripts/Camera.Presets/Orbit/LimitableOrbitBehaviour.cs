ea633c790e3fd300b89c78b1969ba80238b57620
BeardedPlatypus <mwtegelaers@gmail.com>
2021-11-07 21:28:06 +0100

    Add LimitableOrbitBehaviour

---
+++ Assets/Scripts/Camera.Presets/Orbit/LimitableOrbitBehaviour.cs
@@ -0,0 +1,85 @@
+﻿using BeardedPlatypus.Camera.Core;
+using BeardedPlatypus.Camera.Presets.Orbit;
+using UnityEngine;
+
+namespace Camera.Presets.Orbit
+{
+    /// <summary>
+    /// <see cref="LimitableOrbitBehaviour"/> implements the <see cref="IOrbitBehaviour"/>
+    /// where the rotation around the x and y axis can be limited by the values contained in
+    /// the <see cref="ILimitableOrbitSettings"/>.
+    /// </summary>
+    public sealed class LimitableOrbitBehaviour : IOrbitBehaviour
+    {
+        private readonly ILimitableOrbitSettings _settings;
+
+        /// <summary>
+        /// Creates a new <see cref="LimitableOrbitBehaviour"/> with the given
+        /// <paramref name="settings"/>.
+        /// </summary>
+        /// <param name="settings">The settings which define the behaviour.</param>
+        public LimitableOrbitBehaviour(ILimitableOrbitSettings settings)
+        {
+            _settings = settings;
+        }
+        
+        /// <inheritdoc cref="IOrbitBehaviour"/>>
+        public void OnOrbit(Vector2 inputDirection, 
+                            IOrbitCenter orbitCenter, 
+                            Transform cameraTransform)
+        {
+            Vector2 rotationInput = inputDirection * _settings.Factor;
+            RotateAroundX(rotationInput.y,
+                          orbitCenter.Location,
+                          cameraTransform);
+            RotateAroundY(rotationInput.x,
+                          orbitCenter.Location,
+                          cameraTransform);
+        }
+
+        private void RotateAroundX(float rotation, 
+                                   Vector3 orbitCenter, 
+                                   Transform cameraTransform)
+        {
+            var worldX = cameraTransform.TransformVector(Vector3.left);
+            var currentRotation = CalculateCameraRotationAroundX(cameraTransform);
+            
+            // Note the negative sign of the newRotation, this is necessary to ensure we
+            // rotate in the same direction as the mouse movement.
+            float clampedRotation = Mathf.Clamp(-rotation * Mathf.Deg2Rad,
+                                                -currentRotation + _settings.Limit.X.Min * Mathf.Deg2Rad,
+                                                _settings.Limit.X.Max * Mathf.Deg2Rad - currentRotation) 
+                * -Mathf.Rad2Deg;
+            
+            cameraTransform.RotateAround(orbitCenter, worldX, clampedRotation);
+        }
+        
+        private static float CalculateCameraRotationAroundX(Transform cameraTransform)
+        {
+            Vector3 position = cameraTransform.position;
+            float distance = Vector3.Distance(position, Vector3.zero);
+            return Mathf.Asin(position.y / distance);
+        }
+        
+        private void RotateAroundY(float rotation, 
+                                   Vector3 orbitCenter, 
+                                   Transform cameraTransform)
+        {
+            float currentRotation = CalculateCameraRotationAroundY(cameraTransform);
+            float min = -currentRotation + _settings.Limit.Y.Min;
+            float max = _settings.Limit.Y.Max - currentRotation;
+
+            float clampedRotation = Mathf.Clamp(rotation, min, max);
+            cameraTransform.RotateAround(orbitCenter, Vector3.up, clampedRotation);
+        }
+
+        private static float CalculateCameraRotationAroundY(Transform cameraTransform)
+        {
+            float rot = cameraTransform.rotation.eulerAngles.y;
+            // We evaluate the range between -180F and 180F, however
+            // The euler values will be reported between 0 - 360, as 
+            // such we need shift our scale.
+            return rot <= 180F ? rot : rot - 360.0F;
+        }
+    }
+}
---
+++ Assets/Scripts/Camera.Presets/Orbit/LimitableOrbitSettingsScriptableObject.cs
@@ -0,0 +1,48 @@
+﻿using BeardedPlatypus.Camera.Presets.Orbit;
+using UnityEngine;
+
+namespace Camera.Presets.Orbit
+{
+    /// <summary>
+    /// <see cref="LimitableOrbitSettingsScriptableObject"/> provides the
+    /// <see cref="ILimitableOrbitSettings"/> as a <see cref="ScriptableObject"/>
+    /// to be used within the camera setup.
+    /// </summary>
+    [CreateAssetMenu(fileName="LimitableOrbitSettings", 
+                     menuName="BeardedPlatypus/Camera/LimitableOrbitSettings")]
+    public sealed class LimitableOrbitSettingsScriptableObject : ScriptableObject, 
+                                                                 ILimitableOrbitSettings
+    {
+        /// <summary>
+        /// The orbit factor with which orbiting rotation is scaled.
+        /// </summary>
+        [SerializeField] private float factor = 0.05F;
+        
+        /// <summary>
+        /// The smallest allowed rotation around the x-axis in degrees.
+        /// </summary>
+        [SerializeField] private float limitXMin = -90.0F;
+        
+        /// <summary>
+        /// The largest allowed rotation around the x-axis in degrees.
+        /// </summary>
+        [SerializeField] private float limitXMax = 90.0F;
+        
+        /// <summary>
+        /// The smallest allowed rotation around the y-axis in degrees.
+        /// </summary>
+        [SerializeField] private float limitYMin = float.NegativeInfinity;
+        
+        /// <summary>
+        /// The largest allowed rotation around the y-axis in degrees.
+        /// </summary>
+        [SerializeField] private float limitYMax = float.PositiveInfinity;
+
+        /// <inheritdoc cref="ILimitableOrbitSettings" />
+        public float Factor => factor;
+
+        /// <inheritdoc cref="ILimitableOrbitSettings" />
+        public ((float Min, float Max) X, (float Min, float Max) Y) Limit =>
+            ((limitXMin, limitXMax), (limitYMin, limitYMax));
+    }
+}
