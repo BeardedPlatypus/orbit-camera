e8249d0a69a2f32f34be3aac10062fd1339b9b49
BeardedPlatypus <mwtegelaers@gmail.com>
2021-11-07 21:23:54 +0100

    Add OrbitCenter

---
+++ Assets/Scripts/Camera.Core/IOrbitCenter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace BeardedPlatypus.Camera.Core
+{
+    /// <summary>
+    /// <see cref="IOrbitCenter"/> defines the orbit center of a camera.
+    /// </summary>
+    public interface IOrbitCenter
+    {
+        /// <summary>
+        /// Gets the location of this <see cref="IOrbitCenter"/>.
+        /// </summary>
+        Vector3 Location { get; }
+
+        /// <summary>
+        /// Translate this <see cref="IOrbitCenter"/> by the specified
+        /// <see cref="translation"/>.
+        /// </summary>
+        /// <param name="translation"></param>
+        void TranslateBy(Vector3 translation);
+
+        void MoveTo(Vector3 newOrbitCenter);
+    }
+}
---
+++ Assets/Scripts/Camera.Core/OrbitCenter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace BeardedPlatypus.Camera.Core
+{
+    /// <summary>
+    /// <see cref="OrbitCenter"/> implements <see cref="IOrbitCenter"/> as a
+    /// <see cref="MonoBehaviour"/>.
+    /// </summary>
+    public sealed class OrbitCenter : MonoBehaviour, IOrbitCenter
+    {
+        /// <inheritdoc cref="IOrbitCenter"/>
+        public Vector3 Location => transform.position;
+
+        /// <inheritdoc cref="IOrbitCenter"/>
+        public void TranslateBy(Vector3 translation) =>
+            transform.Translate(translation, Space.World);
+
+        /// <inheritdoc cref="IOrbitCenter"/>
+        public void MoveTo(Vector3 newOrbitCenter) =>
+            transform.position = newOrbitCenter;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(transform.position, 
+                                new Vector3(0.05F, 0.05F, 0.05F));
+        }
+    }
+}
