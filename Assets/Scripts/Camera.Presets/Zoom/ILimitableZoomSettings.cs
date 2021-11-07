3f3427d670d0f46aa6e005a6df276858170b75bc
BeardedPlatypus <mwtegelaers@gmail.com>
2021-11-07 21:29:16 +0100

    Add LimitableZoomBehaviour

---
+++ Assets/Scripts/Camera.Presets/Zoom/ILimitableZoomSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace Camera.Presets.Zoom
+{
+    /// <summary>
+    /// <see cref="ILimitableZoomSettings"/> defines the settings related to the zoom behaviour
+    /// of the Camera.
+    /// </summary>
+    public interface ILimitableZoomSettings
+    {
+        /// <summary>
+        /// Gets the zoom factor.
+        /// </summary>
+        public float Factor { get; }
+        
+        /// <summary>
+        /// Gets the range of valid values in world space distance.
+        /// </summary>
+        /// <remarks>
+        /// It is expected that Min is a smaller value than Max and that both values
+        /// are greater than or equal to zero.
+        /// </remarks>
+        public (float Min, float Max) Limit { get; }
+    }
+}
---
+++ Assets/Scripts/Camera.Presets/Zoom/LimitableZoomBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using BeardedPlatypus.Camera.Core;
+using UnityEngine;
+
+namespace Camera.Presets.Zoom
+{
+    /// <summary>
+    /// <see cref="LimitableZoomBehaviour"/> implements the <see cref="IZoomBehaviour"/>
+    /// where the zoom can be limited by the values contained in the
+    /// <see cref="ILimitableZoomSettings"/>.
+    /// </summary>
+    public sealed class LimitableZoomBehaviour : IZoomBehaviour
+    {
+        private readonly ILimitableZoomSettings _settings;
+        
+        /// <summary>
+        /// Creates a new <see cref="LimitableZoomBehaviour"/> with the given
+        /// <see cref="ILimitableZoomSettings"/>.
+        /// </summary>
+        /// <param name="settings"></param>
+        public LimitableZoomBehaviour(ILimitableZoomSettings settings)
+        {
+            _settings = settings;
+        }
+        
+        /// <inheritdoc cref="IZoomBehaviour"/>
+        public void OnZoom(float inputZoom, IOrbitCenter orbitCenter, Transform cameraTransform)
+        {
+            float currDistance = Vector3.Distance(cameraTransform.position, orbitCenter.Location);
+            float minTranslationDistance = _settings.Limit.Min - currDistance;
+            float maxTranslationDistance = _settings.Limit.Max - currDistance;
+            float inputTranslation = -inputZoom * _settings.Factor;
+
+            float translation = Mathf.Clamp(
+                inputTranslation,
+                minTranslationDistance,
+                maxTranslationDistance
+            );
+            
+            cameraTransform.Translate(new Vector3(0F, 0F, -translation));
+        }
+    }
+}
