066ca826f6eeb47dbfb97135fa9f53c4d17bce11
BeardedPlatypus <mwtegelaers@gmail.com>
2021-11-08 16:14:26 +0100

    Add SetOrbitBehaviour

---
+++ Assets/Scripts/Camera.Presets/SetOrbit/SetOrbitBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using BeardedPlatypus.Camera.Core;
+using BeardedPlatypus.Camera.Core.Behaviours;
+using BeardedPlatypus.Camera.Presets.Common;
+using UnityEngine;
+
+namespace BeardedPlatypus.Camera.Presets.SetOrbit
+{
+    /// <summary>
+    /// <see cref="SetOrbitBehaviour"/> provides the default implementation
+    /// of the <see cref="ISetOrbitBehaviour"/>. It will rotate the camera
+    /// to the specified rotation.
+    /// </summary>
+    public class SetOrbitBehaviour : ISetOrbitBehaviour
+    {
+        /// <inheritdoc cref="ISetOrbitBehaviour"/>>
+        public void OnSetOrbit(Vector2 rotation, IOrbitCenter orbitCenter, Transform cameraTransform)
+        {
+            RotateAroundX(rotation.x, orbitCenter.Location, cameraTransform);
+            RotateAroundY(rotation.y, orbitCenter.Location, cameraTransform);
+        }
+
+        private static void RotateAroundX(float rotation, Vector3 orbitCenter, Transform cameraTransform)
+        {
+            Vector3 axis = cameraTransform.TransformVector(Vector3.right);
+            float currentRotation = RotationCalculator.CalculateRotationAroundX(cameraTransform.position,
+                                                                                orbitCenter);
+            cameraTransform.RotateAround(orbitCenter, axis, rotation - currentRotation);
+        }
+
+        private static void RotateAroundY(float rotation, Vector3 orbitCenter, Transform cameraTransform) => 
+            cameraTransform.RotateAround(orbitCenter, 
+                                         Vector3.up, 
+                                         rotation - cameraTransform.eulerAngles.y);
+    }
+}
