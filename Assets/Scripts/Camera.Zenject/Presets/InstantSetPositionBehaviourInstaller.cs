01889c9107a7b9cede2bd3269ee1a0c736c5af65
BeardedPlatypus <mwtegelaers@gmail.com>
2021-11-08 18:05:22 +0100

    Add Set behaviour installers

---
+++ Assets/Scripts/Camera.Zenject/Presets/InstantSetPositionBehaviourInstaller.cs
@@ -0,0 +1,21 @@
+﻿using BeardedPlatypus.Camera.Core.Behaviours;
+using BeardedPlatypus.Camera.Presets.SetPosition;
+using Zenject;
+
+namespace BeardedPlatypus.Camera.Zenject.Presets
+{
+    /// <summary>
+    /// <see cref="InstantSetPositionBehaviourInstaller"/> provides the
+    /// <see cref="ISetPositionBehaviour"/> dependency as a
+    /// <see cref="InstantSetPositionBehaviour"/>.
+    /// </summary>
+    public class InstantSetPositionBehaviourInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<ISetPositionBehaviour>()
+                     .To<InstantSetPositionBehaviour>()
+                     .AsSingle();
+        }
+    }
+}
---
+++ Assets/Scripts/Camera.Zenject/Presets/InstantSetZoomBehaviourInstaller.cs
@@ -0,0 +1,21 @@
+﻿using BeardedPlatypus.Camera.Core.Behaviours;
+using BeardedPlatypus.Camera.Presets.SetZoom;
+using Zenject;
+
+namespace BeardedPlatypus.Camera.Zenject.Presets
+{
+    /// <summary>
+    /// <see cref="InstantSetZoomBehaviourInstaller"/> provides the
+    /// <see cref="ISetZoomBehaviour"/> dependency as a
+    /// <see cref="InstantSetZoomBehaviour"/>.
+    /// </summary>
+    public class InstantSetZoomBehaviourInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<ISetZoomBehaviour>()
+                     .To<InstantSetZoomBehaviour>()
+                     .AsSingle();
+        }
+    }
+}
