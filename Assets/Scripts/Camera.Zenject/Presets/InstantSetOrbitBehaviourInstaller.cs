f1cff0cefd43500ddd313e12b9d707ed14dd9286
BeardedPlatypus <mwtegelaers@gmail.com>
2021-11-08 16:58:13 +0100

    Add InstantSetOrbitBehaviourInstaller

---
+++ Assets/Scripts/Camera.Zenject/Presets/InstantSetOrbitBehaviourInstaller.cs
@@ -0,0 +1,20 @@
+﻿using BeardedPlatypus.Camera.Core.Behaviours;
+using BeardedPlatypus.Camera.Presets.SetOrbit;
+using Zenject;
+
+namespace BeardedPlatypus.Camera.Zenject.Presets
+{
+    /// <summary>
+    /// <see cref="InstantSetOrbitBehaviourInstaller"/> provides the <see cref="ISetOrbitBehaviour"/>
+    /// dependency as a <see cref="InstantSetOrbitBehaviour"/>.
+    /// </summary>
+    public sealed class InstantSetOrbitBehaviourInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<ISetOrbitBehaviour>()
+                     .To<InstantSetOrbitBehaviour>()
+                     .AsSingle();
+        }
+    }
+}
