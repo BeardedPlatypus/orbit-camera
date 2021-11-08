35c674ad5cd93d8cde4f79782b6259574a5c7ffa
BeardedPlatypus <mwtegelaers@gmail.com>
2021-11-08 20:20:35 +0100

    Add ICoroutineRunner to run coroutines in non monobehaviours

---
+++ Assets/Scripts/Camera.Core/ICoroutineRunner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace BeardedPlatypus.Camera.Core
+{
+    /// <summary>
+    /// <see cref="ICoroutineRunner"/> is a convenience interface to run
+    /// co-routines from non-monobehaviour classes.
+    /// </summary>
+    public interface ICoroutineRunner
+    {
+        /// <summary>
+        /// Start the provided <paramref name="coroutine"/>.
+        /// </summary>
+        /// <param name="coroutine">The coroutine to run.</param>
+        Coroutine Run(IEnumerator coroutine);
+    }
+}
