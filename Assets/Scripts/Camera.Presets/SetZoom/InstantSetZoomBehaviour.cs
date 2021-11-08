bf5390c25b8f6dab404d8f419543935d49400594
BeardedPlatypus <mwtegelaers@gmail.com>
2021-11-08 18:01:01 +0100

    Add InstantSetZoomBehaviour

---
+++ Assets/Scripts/Camera.Presets/SetZoom/InstantSetZoomBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using BeardedPlatypus.Camera.Core;
+using BeardedPlatypus.Camera.Core.Behaviours;
+using UnityEngine;
+
+namespace BeardedPlatypus.Camera.Presets.SetZoom
+{
+    /// <summary>
+    /// <see cref="InstantSetZoomBehaviour"/> provides the default implementation
+    /// of the <see cref="ISetZoomBehaviour"/>. It will rotate the camera
+    /// to the specified rotation immediately.
+    /// </summary>
+    public class InstantSetZoomBehaviour : ISetZoomBehaviour
+    {
+        public void OnSetZoom(float zoom, IOrbitCenter orbitCenter, Transform cameraTransform)
+        {
+            float currDistance = Vector3.Distance(cameraTransform.position, orbitCenter.Location);
+            cameraTransform.Translate(new Vector3(0F, 0F, zoom - currDistance));
+        }
+    }
+}
