f293d8707e7812f659eea541f44f1c1929f8ee3c
BeardedPlatypus <mwtegelaers@gmail.com>
2021-11-09 19:32:04 +0100

    Add SmoothSetZoomBehaviourInstaller

---
+++ Assets/Scripts/Camera.Zenject/Presets/SmoothSetZoomBehaviourInstaller.cs
@@ -0,0 +1,27 @@
+﻿using BeardedPlatypus.Camera.Core.Behaviours;
+using BeardedPlatypus.Camera.Presets.SetZoom;
+using UnityEngine;
+using Zenject;
+
+namespace BeardedPlatypus.Camera.Zenject.Presets
+{
+    /// <summary>
+    /// <see cref="SmoothSetZoomBehaviourInstaller"/> provides the
+    /// <see cref="ISetZoomBehaviour"/> dependency as a
+    /// <see cref="SmoothSetZoomBehaviour"/>.
+    /// </summary>
+    public sealed class SmoothSetZoomBehaviourInstaller : MonoInstaller
+    {
+        [SerializeField] private float maxTransitionTime = 0.5F;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<float>()
+                     .FromInstance(maxTransitionTime)
+                     .WhenInjectedInto<SmoothSetZoomBehaviour>();
+            Container.Bind<ISetZoomBehaviour>()
+                     .To<SmoothSetZoomBehaviour>()
+                     .AsSingle();
+        }
+    }
+}
