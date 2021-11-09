5a33d66989f20b80d8e74e11afd89a44f8578b28
BeardedPlatypus <mwtegelaers@gmail.com>
2021-11-09 19:30:42 +0100

    Add SmoothSetPositionBehaviour

---
+++ Assets/Scripts/Camera.Presets/SetPosition/SmoothSetPositionBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using BeardedPlatypus.Camera.Core;
+using BeardedPlatypus.Camera.Core.Behaviours;
+using UnityEngine;
+
+namespace BeardedPlatypus.Camera.Presets.SetPosition
+{
+    /// <summary>
+    /// <see cref="SmoothSetPositionBehaviour"/> sets the provided position over
+    /// the specified transition time.
+    /// </summary>
+    public sealed class SmoothSetPositionBehaviour : ISetPositionBehaviour
+    {
+        private readonly float _transitionTime;
+        private readonly ICoroutineRunner _runner;
+
+        /// <summary>
+        /// Creates a new <see cref="SmoothSetPositionBehaviour"/> with the given
+        /// dependencies.
+        /// </summary>
+        /// <param name="transitionTime">The transition time.</param>
+        /// <param name="runner">The coroutine runner.</param>
+        public SmoothSetPositionBehaviour(float transitionTime, ICoroutineRunner runner)
+        {
+            _transitionTime = transitionTime;
+            _runner = runner;
+        }
+
+        /// <inheritdoc cref="ISetPositionBehaviour.OnSetPosition"/>
+        public void OnSetPosition(Vector3 position, IOrbitCenter orbitCenter, Transform cameraTransform) =>
+            _runner.Run(TranslateSmooth(position, orbitCenter, cameraTransform));
+
+        private IEnumerator TranslateSmooth(Vector3 goalOrbitPosition,
+                                            IOrbitCenter orbitCenter,
+                                            Transform cameraTransform)
+        {
+            var orbitTranslation = goalOrbitPosition - orbitCenter.Location;
+            float timeFactor = 1.0F / _transitionTime;
+            
+            for (var t = 0.0F; t < 1.0F; t += Time.deltaTime * timeFactor)
+            {
+                var localTranslation = Time.deltaTime * timeFactor * orbitTranslation;
+                cameraTransform.Translate(localTranslation, Space.World);
+                orbitCenter.TranslateBy(localTranslation);
+                yield return null;
+            }
+        }
+    }
+}
